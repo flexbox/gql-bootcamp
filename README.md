22d5c84f43c3aff4e8c2d2bc9dc28d2bc7ab197c
David Leuliette <dleuliette@gmail.com>
2020-03-08 17:22:09 +0100

    step13

--- README.md
+++ README.md
@@ -1,3 +1,31 @@
-Write an HTTP server that receives only POST requests and converts incoming POST body characters to upper-case and returns it to the client.
+Write an HTTP server that serves JSON data when it receives a GET request
+to the path `/api/parsetime`. Expect the request to contain a query string
+with a key `iso` and an ISO-format time as the value.
+
+For example:
+
+```
+/api/parsetime?iso=2013-08-10T12:10:15.474Z
+```
+
+The JSON response should contain only 'hour', 'minute' and 'second' properties.
+
+For example:
+
+```json
+{
+  "hour": 14,
+  "minute": 23,
+  "second": 15
+}
+```
+
+Add second endpoint for the path `/api/unixtime` which accepts the same query string but returns UNIX epoch time in milliseconds (the number of milliseconds since 1 Jan 1970 00:00:00 UTC) under the property `unixtime`.
+
+For example:
+
+```json
+{ "unixtime": 1376136615474 }
+```
 
 Your server should listen on the port provided by the first argument to your program.
---
+++ http-json-api-server.js
@@ -0,0 +1,45 @@
+const http = require('http');
+const param = process.argv[2];
+
+function getTime(time) {
+  return {
+    hour: time.getHours(),
+    minute: time.getMinutes(),
+    second: time.getSeconds()
+  };
+}
+
+function getUnixTime(time) {
+  return { unixtime: time.getTime() };
+}
+
+http
+  .createServer(function(request, response) {
+    const parsedUrl = new URL(request.url, 'http://example.com');
+    const time = new Date(parsedUrl.searchParams.get('iso'));
+    let result = undefined;
+
+    console.log('parsedUrl', parsedUrl);
+    console.log('time', time);
+    console.log('request.url', request.url);
+
+    if (parsedUrl.pathname === '/api/parsetime') {
+      result = getTime(time);
+    }
+    if (parsedUrl.pathname === '/api/unixtime') {
+      result = getUnixTime(time);
+    }
+
+    if (result) {
+      response.writeHead(200, { 'Content-Type': 'application/json' });
+      response.end(JSON.stringify(result));
+    } else {
+      response.writeHead(404);
+      response.end('Not found');
+    }
+  })
+  .listen(Number(param));
+
+console.log(
+  `\nServer running at http://127.0.0.1:${param}/api/parsetime?iso=2020-03-08T16:05:40.030Z`
+);
